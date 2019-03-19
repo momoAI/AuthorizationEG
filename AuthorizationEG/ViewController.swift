8e2ac6b8c98f36e4e46eac58154313f9b20ad868
momo <luxu@yanfabu-luxu.local>
2019-03-19 16:25:37 +0800

    Initial Commit

---
+++ AuthorizationEG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AuthorizationEG
+//
+//  Created by luxu on 2019/3/19.
+//  Copyright © 2019年 lx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
